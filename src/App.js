cb4bf77ab17ea5ccc3b5e2ac646fdf73701e0d0e
buiphuking <buiphuking@gmail.com>
2022-06-19 15:39:29 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "reactstrap";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className='App'>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
